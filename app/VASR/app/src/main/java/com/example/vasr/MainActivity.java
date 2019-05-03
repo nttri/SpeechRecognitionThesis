97586ff29849a0fcdb60ac102798dddcde415353
Nguyễn Thanh Trí <trint4@vng.com.vn>
2019-05-03 11:03:28 +0700

    [App] add method to call API

--- app/VASR/app/src/main/java/com/example/vasr/MainActivity.java
+++ app/VASR/app/src/main/java/com/example/vasr/MainActivity.java
@@ -1,13 +1,132 @@
 package com.example.vasr;
 
+import android.media.MediaPlayer;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnRecord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRecord = findViewById(R.id.btn_record);
+        btnRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postRequest();
+            }
+        });
     }
+
+    private void postRequest() {
+        InputStream is = getResources().openRawResource(R.raw.voice001);
+        File file = new File(getCacheDir(), "voice001.wav");;
+        OutputStream output;
+        try {
+            output = new FileOutputStream(file);
+            try {
+                byte[] buffer = new byte[4 * 1024]; // or other buffer size
+                int read;
+
+                while ((read = is.read(buffer)) != -1) {
+                    output.write(buffer, 0, read);
+                }
+
+                output.flush();
+            } finally {
+                output.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        step2(file);
+//        String fn = file.getName();
+//        int file_size = Integer.parseInt(String.valueOf(file.length()/1024));
+//
+//        System.out.println();
+    }
+
+    private void step2(File file) {
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://vnstt001.herokuapp.com/file";
+
+        RequestBody body = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("voice",file.getName(),RequestBody.create(MediaType.parse("audio/wav"),file))
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+//        try {
+//            Response response = client.newCall(request).execute();
+//            String rs = response.body().string();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String mMessage = e.getMessage().toString();
+                Log.w("failure Response", mMessage);
+                //call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                String mMessage = response.body().string();
+                if (response.isSuccessful()){
+                    try {
+                        JSONObject json = new JSONObject(mMessage);
+                        String serverResponse = json.getString("text");
+                        System.out.println();
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+
+    }
+
 }
