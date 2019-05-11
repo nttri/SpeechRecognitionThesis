e2a8290245ad6ab81a91f840ab3ac923daf47fac
Nguyễn Thanh Trí <trint4@vng.com.vn>
2019-05-11 08:37:18 +0700

    [App] add back button in ResultActivity

--- app/VASR/app/src/main/java/com/example/vasr/ResultActivity.java
+++ app/VASR/app/src/main/java/com/example/vasr/ResultActivity.java
@@ -5,6 +5,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,22 +32,33 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        getWindow().getDecorView().setBackgroundColor(Color.rgb(238,238,238));
 
         editText = findViewById(R.id.editText);
         btnExportPDF = findViewById(R.id.btnExportPDF);
 
-        editText.setMovementMethod(new ScrollingMovementMethod());
-        editText.setBackgroundColor(Color.WHITE);
+        setupUI();
+    }
 
-        btnExportPDF.setBackgroundColor(Color.RED);
-        btnExportPDF.setTextColor(Color.WHITE);
-
+    private void setupUI() {
+        //set title on toolbar
         getSupportActionBar().setTitle("Kết quả dịch");
 
+        //set activity background color
+        getWindow().getDecorView().setBackgroundColor(Color.rgb(238,238,238));
+
+        //add back button on toolbar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //setup edittext
+        editText.setMovementMethod(new ScrollingMovementMethod());
+        editText.setBackgroundColor(Color.WHITE);
         resultText = getIntent().getStringExtra("TEXT");
         editText.setText(resultText);
 
+        //setup export PDF button
+        btnExportPDF.setBackgroundColor(Color.RED);
+        btnExportPDF.setTextColor(Color.WHITE);
         btnExportPDF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,5 +82,16 @@ public class ResultActivity extends AppCompatActivity {
         } catch (Exception e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //handle user click on button back
+        if (id == android.R.id.home) {
+            //close activity
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
